4567392fad8a1e9a5be2c13670a6b318756199d8
Gabriel Lakatos <gabriellakatos@gmail.com>
2023-08-27 19:58:35 -0300

    first commit

---
+++ backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento(){
+                EventoId = 1,
+                Tema = ".NET e Angular",
+                Local = "Barueri",
+                Lote = "1º Lote",
+                QtdPessoas = 100,
+                DataEvento = DateTime.Now.ToString()
+            };
+        }
+    }
+}
